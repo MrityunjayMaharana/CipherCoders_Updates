f4b8068a61c9ce098e33683b6e258d274dd24b32
Mrityunjay Maharana <115680134+MrityunjayMaharana@users.noreply.github.com>
2024-12-04 21:03:40 +0530

    Update assignment_02.cpp

--- assignment_02.cpp
+++ assignment_02.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
 
+/*
+    Beginner level - Question - 02
+    Ques 1. Write a C++ program that takes three integers as input from the user and determines the largest of the three using if-else statements.
+
+    Sample Input:- Enter the first number: 10
+    Enter the second number: 20
+    Enter the third number: 15
+
+    Sample Output :- The largest number is: 20
+
+    Ques 2. Write a C++ program that takes an integer score (0-100) as input from the user and prints the corresponding grade using if-else-if statements.
+    Grade Criteria:
+    A: 90-100
+    B: 80-89
+    C: 70-79
+    D: 60-69
+    F: 0-59
+    Sample Input:- Enter the score: 85
+    Sample Output:- Grade: B
+
+    Ques 3. Write a C++ program that takes an integer as input from the user and prints the corresponding month of the year using a switch case statement. Assume 1 = January, 2 = February, ..., 12 = December.If integer is not from 1 to 12 then print invalid.
+
+    Sample Input:-Enter a number : 5
+    Sample Output :- The month is: May
+*/
+
 int main() {
     // 01: Find the largest of three numbers
     int num1, num2, num3;
